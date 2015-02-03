5e0175f7f8f14c312f894380426ccbeafa24d854
Charles Vaughn <cvaughn@gmail.com>
2015-02-02 17:27:20 -0800

    Working.

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ pixelScale = 20
 import pygame, sys
 from pygame.locals import *
 import numpy as np
+import random
 import time
 import math
 
@@ -14,7 +15,9 @@ import math
 pygame.init()
 
 # set up the window
-screen = pygame.display.set_mode((width * pixelScale, height * pixelScale), 0, 32)
+real_screen = pygame.display.set_mode((width * pixelScale, height * pixelScale), 0, 32)
+
+screen = pygame.Surface((64, 64))
 
 pygame.joystick.init()
 joysticks = [pygame.joystick.Joystick(x) for x in range(pygame.joystick.get_count())]
@@ -43,59 +46,108 @@ snake = [np.array([5,10])]
 
 snakeLength = 10
 
+fruits = [np.array([20,20])]
+
+def drawRect(x, y, color):
+    screen.set_at((int(x),int(y)), color)
+
 def drawSnake():
     for s in snake:
-        pygame.draw.rect(screen, snakeColor,
-                         (s[0] * pixelScale, s[1] * pixelScale, pixelScale, pixelScale)
-        )
+        drawRect(s[0], s[1], snakeColor)
+
 def drawWalls():
     for w in walls:
-        pygame.draw.rect(screen, (255,0,0),
-                         (w[0] * pixelScale, w[1] * pixelScale, pixelScale, pixelScale)
-        )
+        drawRect(w[0], w[1], (255,0,0))
+
+
+def drawFruit():
+    for f in fruits:
+        drawRect(f[0], f[1], (255, 255, 0))
 
 def didDie():
     global snake
     global isAlive
     global snakeColor
+
     for w in walls:
         if np.array_equal(w, snake[-1]):
             isAlive = False
             snakeColor = (238,130,238)
             return
 
-while True:
-    screen.fill((0,0,0))
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            pygame.quit()
-            sys.exit()
+    if len(snake) > 10:
+        for s in snake[:-1]:
+            if np.array_equal(s, snake[-1]):
+                isAlive = False
+                snakeColor = (238,130,238)
+                return
 
-        if event.type == JOYAXISMOTION:
-            if event.value != 0:
-                snakeVel = [0, 0]
-                snakeVel[event.axis] = math.copysign(1, event.value)
+def caughtFruit():
+    global snakeLength
 
+    for i in range(len(fruits)):
+        if np.array_equal(fruits[i], snake[-1]):
+            fruits[i] = np.array([random.randint(1, 62),random.randint(1, 62)])
+            snakeLength += 5
 
-        if event.type == JOYBUTTONDOWN:
-            isAlive = True
-            snakeColor = (0,255,128)
-            snake = [np.array([5,10])]
+def main():
+    global isAlive
+    global snake
+    global snakeVel
+    global snakeColor
+    global snakeLength
 
-    didDie()
-
-    drawWalls()
-
-    drawSnake()
-
-    pygame.display.update()
-
-    if isAlive:
-        newPos = snake[-1] + snakeVel
-        snake.append(newPos)
-
-    if len(snake) > snakeLength:
-        snake = snake[1:]
+    font = pygame.font.Font("5x5_pixel.ttf", 8)
 
 
-    time.sleep(0.15)
+    while True:
+        oldVel = snakeVel
+        screen.fill((0,0,0))
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+
+            if event.type == JOYAXISMOTION:
+                if math.fabs(event.value) > 0.5 and oldVel[event.axis % 2] == 0:
+                    snakeVel = [0, 0]
+                    snakeVel[event.axis % 2] = math.copysign(1, event.value)
+
+
+            if event.type == JOYBUTTONDOWN:
+                isAlive = True
+                snakeColor = (0,255,128)
+                snake = [np.array([5,10])]
+                snakeVel = np.array([0,0])
+                snakeLength = 10
+
+        didDie()
+
+        drawWalls()
+
+        drawSnake()
+
+        text = font.render(str((snakeLength - 10) / 5), False, (255, 255, 255))
+
+        screen.blit(text, (50,2))
+
+        drawFruit()
+
+        caughtFruit()
+
+        pygame.transform.scale(screen, (64 * pixelScale, 64 * pixelScale), real_screen)
+
+        pygame.display.update()
+
+        if isAlive:
+            newPos = snake[-1] + snakeVel
+            snake.append(newPos)
+
+        if len(snake) > snakeLength:
+            snake = snake[1:]
+
+        time.sleep(0.08)
+
+
+if __name__ == "__main__":
+    main()
